71deb132bfbde1653116e382a51749e2fcb5469c
Jeremy Bobbin <jeremybobbin@gmail.com>
2019-03-19 16:48:30 -0700

    add function that spawns another terminal in pwd

--- .bashrc.d/utils.bash
+++ .bashrc.d/utils.bash
@@ -109,6 +109,9 @@ playtime() {
 	$EDITOR $to_edit
 }
 
+term() {
+	nohup st -c "cd $pwd" &> /dev/null & disown
+}
 
 recurse() {
 	echo $@ | xargs -I{} find {} -type d -not -path '*/\.*' | tr '\n' ':' | sed -e 's/:$//'
