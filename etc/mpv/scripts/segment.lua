46848c877f17b88bf099930669ce4f7c66537c88
Jeremy Bobbin <jer@jer.cx>
2021-01-26 02:46:57 -0800

    mpv: rework segment.lua script

--- etc/mpv/scripts/segment.lua
+++ etc/mpv/scripts/segment.lua
@@ -1,28 +1,27 @@
-local clip_start
-local uri 
+local keep_open
+local start = nil
 
-function set_clip_start()
-	clip_start = mp.get_property_number("time-pos")
-	local message = string.format("Start of clip: %s", clip_start)
-	mp.osd_message(message)
-end
-
-function clip_end()
-	local clip_end = mp.get_property_number("time-pos")
-	local offset = clip_end - clip_start
-	local uri = mp.get_property("path")
-
-	local ffmpeg = string.format("ffmpeg -i %q -ss %s -t %s ~/Media/Video/Segments/$(date +%%s).mp4", uri, clip_start, offset)
-	os.execute(ffmpeg)
-end
-
-function segment() 
-	if not clip_start then
-		set_clip_start()
+mp.add_key_binding("s", "segment", function()
+	if start then
+		local offset = mp.get_property_number("time-pos") - start
+		if offset < 0 then
+			mp.osd_message(string.format("start before end - cancelling"))
+			start = nil
+			return
+		end
+		local path = mp.get_property("path")
+		local ext = path:match("^.+%.(.+)$")
+		local ffmpeg = string.format("ffmpeg -i %q -ss %f -t %f -c copy ~/Media/Video/Segments/%d.%s", path, start, offset, os.time(), ext)
+		mp.osd_message(string.format("Start: %.2f - Duration: %.2f", start, offset))
+		os.execute(ffmpeg)
+	        mp.set_property("keep-open", keep_open)
 	else
-		clip_end()
-		clip_start = nil
+	        keep_open = mp.get_property("keep-open")
+	        mp.set_property("keep-open", "always")
+		start = mp.get_property_number("time-pos")
+		if start < 0 then
+			start = 0
+		end
+		mp.osd_message(string.format("Start of clip: %s", start))
 	end
-end
-
-mp.add_key_binding("s", "segment", segment)
+end)
