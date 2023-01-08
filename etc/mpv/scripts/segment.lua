929f78afac01abbfd4d2b81f9c393a3abbc5674e
Jeremy Bobbin <jer@jer.cx>
2023-01-08 13:50:21 -0800

    use alternative ffmpeg syntax & fmt

--- etc/mpv/scripts/segment.lua
+++ etc/mpv/scripts/segment.lua
@@ -3,16 +3,24 @@ local start = nil
 
 mp.add_key_binding("s", "segment", function()
 	if start then
-		local offset = mp.get_property_number("time-pos") - start
-		if offset < 0 then
+		local to = mp.get_property_number("time-pos")
+		if to < 0 then
 			mp.osd_message(string.format("start before end - cancelling"))
 			start = nil
 			return
 		end
 		local path = mp.get_property("path")
 		local ext = path:match("^.+%.(.+)$")
-		local ffmpeg = string.format("ffmpeg -i %q -ss %f -t %f -c copy ~/Media/Video/Segments/%d.%s", path, start, offset, os.time(), ext)
-		mp.osd_message(string.format("Start: %.2f - Duration: %.2f", start, offset))
+		local name = path:match("^([^/]+)%..+$")
+		local ffmpeg = string.format("ffmpeg -i %q -ss %.2f -to %.2f ~/Media/Video/Segments/%s-%d.%s",
+			path,
+			start,
+			to,
+			name,
+			os.time(),
+			ext
+		)
+		mp.osd_message(string.format("Start: %.2f - Duration: %.2f", start, to))
 		os.execute(ffmpeg)
 	        mp.set_property("keep-open", keep_open)
 	else
