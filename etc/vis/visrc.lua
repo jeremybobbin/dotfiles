80b3518f48a20922f17bc42c5460af4d7db4d438
Jeremy Bobbin <jer@jer.cx>
2023-03-25 18:42:29 -0700

    make snippets aware of indents

--- etc/vis/visrc.lua
+++ etc/vis/visrc.lua
@@ -12,81 +12,11 @@ dictfiles =  {
 	text = '/usr/share/dict/words',
 }
 
-local register_snippets = function(snippets)
-	for k, v in pairs(snippets) do
-		local seq = string.format("<C-_>%s", k)
-		--v = v:gsub("\n", "<Enter>")
-		vis:map(vis.modes.NORMAL, seq, function(keys)
-			vis:feedkeys("i")
-			vis:insert(v)
-			local str = string.format("<Escape>%dh", string.len(v))
-			vis:feedkeys(str)
-		end, "Snippet")
-		vis:map(vis.modes.INSERT, seq, function(keys)
-			vis:insert(v)
-			vis:feedkeys("<Escape>")
-			vis:feedkeys(" ")
-		end, "Snippet")
-	end
+local function get_indent(win)
+	local sel = win.selection
+	local line = win.file:content(sel.pos-(sel.col-1), sel.col-1)
+	return line:match("^%s+")
 end
-
-
-local on_syntax = {
-	ansi_c = function()
-		register_snippets({
-			po = "fprintf(stdout, \"<++>\", <++>);",
-			pe = "fprintf(stderr, \"<++>\", <++>);",
-			pf = "fprintf(<++>, \"<++>\", <++>);",
-			ps = "sprintf(<++>, \"<++>\", <++>);",
-			-- loop
-			lf = "for (<++>;<++>;<++>) {\n\t<++>\n}",
-			lw = "while (<++>) {\n\t<++>\n}",
-			ld = "do {\n\t<++>\n} while (<++>);",
-
-			["if"] = "if (<++>) {\n\t<++>\n}",
-
-			fn = "<++>\n<++>(<++>)\n{\n\t<++>\n}",
-			sw = "switch (<++>) {\ncase <++>:<++>;\n\tdefault:<++>;;\n}",
-		})
-	end,
-	bash = function()
-		register_snippets({
-			po = "printf '<++>'",
-			pe = "printf '<++>' 1>&2",
-			pf = "printf '<++>' > <++>",
-			-- loop
-			lw = "while <++>; do\n\t<++>\ndone",
-			lu = "until <++>; do\n\t<++>\ndone",
-			lf = "for <++> in <++>; do\n\t<++>\ndone",
-
-			["if"] = "if <++>; then\n\t<++>\nfi",
-			fn = "<++>()\n{\n\t<++>\n}",
-			sw = "case <++> in\n\t<++>)<++>;;\n\t*)<++>;;\nesac",
-			hd = "<++> <<- EOF\n<++>\nEOF",
-
-			ca = "awk '<++> {<++>}'",
-		})
-	end,
-	go = function()
-		register_snippets({
-			po = "fmt.Println(\"<++>\")",
-			pe = "fmt.Fprintln(os.Stderr, \"<++>\", <++>)",
-			pf = "fmt.Fprintln(<++>, \"<++>\")",
-			-- loop
-			lw = "for {\n\t<++>\n}",
-			lr = "for <++> := range <++>; {\n\t<++>\n}",
-			ln = "for i := 0; i < <++>; i+= 1 do\n\t<++>\ndone",
-
-			["if"] = "if <++>; then\n\t<++>\nfi",
-			fn = "func <++>() <++> {\n\t<++>\n}",
-			sw = "switch <++> {\ncase <++>:\n\t<++>\n}",
-			sl = "select <++> {\ncase <++>:\n\t<++>\n}",
-			hd = "<++> <<- EOF\n<++>\nEOF",
-
-			ca = "awk '<++> {<++>}'",
-		})
-	end
-}
 
 -- fancy string ops
 local strdefi=getmetatable('').__index
@@ -140,6 +70,87 @@ vis.events.subscribe(vis.events.WIN_OPEN, function(win)
 	vis:map(vis.modes.VISUAL, "<C-s>W", function(keys)
 		vis:command('x/\\S+/')
 	end, "jump to place holder")
+
+
+	local register_snippets = function(snippets)
+		for k, v in pairs(snippets) do
+			local seq = string.format("<C-_>%s", k)
+			--v = v:gsub("\n", "<enter>")
+			vis:map(vis.modes.NORMAL, seq, function(keys)
+				local indent = get_indent(vis.win)
+				local v = v:gsub("\n", "\n"..(indent or ""))
+				win.file:insert(win.selection.pos, v)
+				local keys = string.format("%dh ", string.len(v))
+				vis:feedkeys(keys)
+			end, "Snippet")
+			vis:map(vis.modes.INSERT, seq, function(keys)
+				local indent = get_indent(vis.win)
+				local v = v:gsub("\n", "\n"..(indent or ""))
+				vis:insert(v)
+				vis:feedkeys("<Escape>")
+				local keys = string.format("%dh ", string.len(v))
+				vis:feedkeys(keys)
+			end, "Snippet")
+		end
+	end
+
+
+	local on_syntax = {
+		ansi_c = function()
+			register_snippets({
+				po = "fprintf(stdout, \"<++>\", <++>);",
+				pe = "fprintf(stderr, \"<++>\", <++>);",
+				pf = "fprintf(<++>, \"<++>\", <++>);",
+				ps = "sprintf(<++>, \"<++>\", <++>);",
+				-- loop
+				lf = "for (<++>;<++>;<++>) {\n\t<++>\n}",
+				lw = "while (<++>) {\n\t<++>\n}",
+				ld = "do {\n\t<++>\n} while (<++>);",
+
+				["if"] = "if (<++>) {\n\t<++>\n}",
+
+				fn = "<++>\n<++>(<++>)\n{\n\t<++>\n}",
+				sw = "switch (<++>) {\ncase <++>:<++>;\n\tdefault:<++>;;\n}",
+			})
+		end,
+		bash = function()
+			register_snippets({
+				po = "printf '<++>'",
+				pe = "printf '<++>' 1>&2",
+				pf = "printf '<++>' > <++>",
+				-- loop
+				lw = "while <++>; do\n\t<++>\ndone",
+				lu = "until <++>; do\n\t<++>\ndone",
+				lf = "for <++> in <++>; do\n\t<++>\ndone",
+
+				["if"] = "if <++>; then\n\t<++>\nfi",
+				fn = "<++>()\n{\n\t<++>\n}",
+				sw = "case <++> in\n\t<++>)<++>;;\n\t*)<++>;;\nesac",
+				hd = "<++> <<- EOF\n<++>\nEOF",
+
+				ca = "awk '<++> {<++>}'",
+			})
+		end,
+		go = function()
+			register_snippets({
+				po = "fmt.Println(\"<++>\")",
+				pe = "fmt.Fprintln(os.Stderr, \"<++>\", <++>)",
+				pf = "fmt.Fprintln(<++>, \"<++>\")",
+				-- loop
+				lw = "for {\n\t<++>\n}",
+				lr = "for <++> := range <++>; {\n\t<++>\n}",
+				ln = "for i := 0; i < <++>; i+= 1 do\n\t<++>\ndone",
+
+				["if"] = "if <++>; then\n\t<++>\nfi",
+				fn = "func <++>() <++> {\n\t<++>\n}",
+				sw = "switch <++> {\ncase <++>:\n\t<++>\n}",
+				sl = "select <++> {\ncase <++>:\n\t<++>\n}",
+				hd = "<++> <<- EOF\n<++>\nEOF",
+
+				ca = "awk '<++> {<++>}'",
+			})
+		end
+	}
 
 	-- snippets
 	if on_syntax[win.syntax] ~= nil then
